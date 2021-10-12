2b3c9a719070d5302ed636706340dfbaaa9fef43
default <=>
2021-10-12 20:48:07 +0400

    Verjin dasi kody

--- js_oop.js
+++ js_oop.js
@@ -4,13 +4,14 @@
 
 // }
 
+
 // const book2 = {
 // 	name : "shdj jsdh",
 // 	pages : 512
 // }
 
 
-function ParentFunction(bookName, bookPages) {
+/*function ParentFunction(bookName, bookPages) {
 
 	this.name = bookName
 	this.pages = bookPages
@@ -24,4 +25,28 @@ const book1 = new ParentFunction("Война и мир", 1024)
 const book2 = Object.create(ParentFunction)
 book2.name_1 = "some_name"
 book2.pages = 234
-console.log(book1.functionName())
+console.log(book1.functionName())*/
+
+class Parent {
+
+	constructor(bookName, bookPages){
+		this.name = bookName
+		this.pages = bookPages
+		
+	}
+	functionName = function(){return console.log(this.name, this.pages)}
+
+}
+class Child extends Parent{
+	constructor(book){
+		super(book) 
+	}
+}
+
+
+
+const exm = new Child("A byte of python")
+console.log(exm.functionName())
+
+
+
